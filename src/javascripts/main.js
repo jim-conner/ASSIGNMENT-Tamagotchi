89ebb06b403c9510d0f56b83ca9eff4f2d7de2d9
Jim Administrator <jamesdavidconner@gamil.com>
2021-02-28 14:28:02 -0600

    added functionality for play quad

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,8 +5,8 @@ const eatAddSub = () => {
   const eatButtonsToDom = `
   <h5 id="eat-title">EAT</h5>
   <h5 id="eat-number"><em>${full}</em></h5>
-  <button id="add-10" type="button" class="btn btn-primary">Healthy</button>
-  <button id="sub-3" type="button" class="btn btn-primary">Unhealthy</button>`;
+  <button id="add-10" type="button" class="btn btn-primary">Apple</button>
+  <button id="sub-3" type="button" class="btn btn-primary">Ice Cream</button>`;
   document.querySelector('#eat').innerHTML += eatButtonsToDom;
   document.querySelector('#eat-number').innerHTML = full;
 
@@ -35,8 +35,46 @@ const eatAddSub = () => {
     }
   });
 };
+
+let fun = 50;
+const play = () => {
+  const playButtonsToDom = `
+  <h5 id="play-title">PLAY</h5>
+  <h5 id="play-number"><em>${fun}</em></h5>
+  <button id="play-add-50" type="button" class="btn btn-primary">Coding</button> 
+  <button id="play-add-2" type="button" class="btn btn-primary">Tea Time</button>`;
+  document.querySelector('#play').innerHTML += playButtonsToDom;
+  document.querySelector('#play-number').innerHTML = fun;
+
+  const playAddFirst = () => {
+    fun += 50;
+    if (fun > 100) {
+      fun = 100;
+    }
+    document.querySelector('#play-number').innerHTML = fun;
+  };
+
+  const playAddSecond = () => {
+    fun += 2;
+    if (fun > 100) {
+      fun = 100;
+    }
+    document.querySelector('#play-number').innerHTML = fun;
+  };
+
+  document.querySelector('#play').addEventListener('click', (e) => {
+    if (e.target.id.includes('play-add-50')) {
+      playAddFirst();
+    }
+    if (e.target.id.includes('play-add-2')) {
+      playAddSecond();
+    }
+  });
+};
+
 const init = () => {
   eatAddSub();
+  play();
 };
 
 init();
